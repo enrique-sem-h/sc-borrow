0b8c0342243a5c8ff4ef720e03aeefbb8daba042
Victor Inácio <42353254+victor-inacio@users.noreply.github.com>
2026-05-20 15:09:57 -0300

    Removes

--- tests/server/unit/controller.test.ts
+++ tests/server/unit/controller.test.ts
@@ -1,137 +1,109 @@
-import handlers, { HandlerMethod }  from '../../../server/handlers/handlers'
-import BaseController from '../../../server/controllers/base-controller'
-import { NextApiRequest, NextApiResponse } from 'next'
-import { expect, test, vi , describe} from 'vitest'
+import handlers, { HandlerMethod } from "../../../server/handlers/handlers";
+import BaseController from "../../../server/controllers/base-controller";
+import { NextApiRequest, NextApiResponse } from "next";
+import { expect, test, vi, describe } from "vitest";
 
-const asdd = []
-
-function Router(method: HandlerMethod, route: string) {
-  return function(ctr: Function) {
-    handlers.addHandler({
-      route: route.split('/'),
-      method
-    })
-  }
-}
-
-function loggedMethod(constructor: Function) {
-  
-}
-
-
-@loggedMethod
 class TestController extends BaseController {
   public exposeUse(func) {
-    this.use(func)
+    this.use(func);
   }
 
   public run(req: any, res: any, ...rest: any[]) {
-    this.handleRequest(req, res, ...rest)
-  }
-
-
-  @Router("GET", "router")
-  public routerTest() {
-
+    this.handleRequest(req, res, ...rest);
   }
 }
 
 describe("BaseController", () => {
+  test("if middlwares are being called correctly", () => {
+    const fn1 = vi.fn((req: NextApiRequest, res: NextApiResponse, next) => {
+      next();
+    });
 
-test('if middlwares are being called correctly', () => {
-  const fn1 = vi.fn((req: NextApiRequest, res: NextApiResponse, next) => {
-    next()
-  })
+    const fn2 = vi.fn((req: NextApiRequest, res: NextApiResponse, next) => {
+      next();
+    });
 
-  const fn2 = vi.fn((req: NextApiRequest, res: NextApiResponse, next) => {
-    next()
-  })
+    const fn3 = vi.fn((req: NextApiRequest, res: NextApiResponse, next) => {
+      next();
+    });
 
-  const fn3 = vi.fn((req: NextApiRequest, res: NextApiResponse, next) => {
-    next()
-  })
+    const testController = new TestController();
 
-  const testController = new TestController()
+    const req = () => {};
+    const res = () => {};
 
-  const req = () => {}
-  const res = () => {}
+    testController.exposeUse(fn1);
+    testController.exposeUse(fn2);
 
-  testController.exposeUse(fn1)
-  testController.exposeUse(fn2)
+    testController.run(req, res, fn3);
 
-  testController.run(req, res, fn3)
+    expect(fn1).toHaveBeenCalledTimes(1);
+    expect(fn2).toHaveBeenCalledTimes(1);
+  });
 
-  expect(fn1).toHaveBeenCalledTimes(1)
-  expect(fn2).toHaveBeenCalledTimes(1)
-})
+  test("if middlwares are being called in correct order", () => {
+    const order: number[] = [];
 
-test('if middlwares are being called in correct order', () => {
+    const fn1 = vi.fn((req: NextApiRequest, res: NextApiResponse, next) => {
+      order.push(1);
+      next();
+    });
 
-  const order: number[] = []
+    const fn2 = vi.fn((req: NextApiRequest, res: NextApiResponse, next) => {
+      order.push(2);
+      next();
+    });
 
-  const fn1 = vi.fn((req: NextApiRequest, res: NextApiResponse, next) => {
-    order.push(1)
-    next()
-  })
+    const fn3 = vi.fn((req: NextApiRequest, res: NextApiResponse, next) => {
+      order.push(3);
+      next();
+    });
 
-  const fn2 = vi.fn((req: NextApiRequest, res: NextApiResponse, next) => {
-    order.push(2)
-    next()
-  })
+    const testController = new TestController();
 
-  const fn3 = vi.fn((req: NextApiRequest, res: NextApiResponse, next) => {
-    order.push(3)
-    next()
-  })
+    const req = () => {};
+    const res = () => {};
 
-  const testController = new TestController()
+    testController.exposeUse(fn1);
+    testController.exposeUse(fn2);
 
-  const req = () => {}
-  const res = () => {}
+    testController.run(req, res, fn3);
 
-  testController.exposeUse(fn1)
-  testController.exposeUse(fn2)
+    expect(order).toEqual([1, 2, 3]);
+  });
+});
 
-  testController.run(req, res, fn3)
-
-  expect(order).toEqual([1, 2, 3])
-
-})
-})
-
-test('if middlwares dont propagate if one fails', () => {
-
-  const order: number[] = []
+test("if middlwares dont propagate if one fails", () => {
+  const order: number[] = [];
 
   const fn1 = vi.fn((req: NextApiRequest, res: NextApiResponse, next) => {
-    order.push(1)
-    next()
-  })
+    order.push(1);
+    next();
+  });
 
   const fn2 = vi.fn((req: NextApiRequest, res: NextApiResponse, next) => {
     // Simulate check error
     if (false) {
-      next()
+      next();
     }
-  })
+  });
 
   const fn3 = vi.fn((req: NextApiRequest, res: NextApiResponse, next) => {
     //This function can't be called because of check error
-  })
+  });
 
-  const testController = new TestController()
+  const testController = new TestController();
 
-  const req = () => {}
-  const res = () => {}
+  const req = () => {};
+  const res = () => {};
 
-  testController.exposeUse(fn1)
-  testController.exposeUse(fn2)
-  testController.exposeUse(fn3)
+  testController.exposeUse(fn1);
+  testController.exposeUse(fn2);
+  testController.exposeUse(fn3);
 
-  testController.run(req, res)
+  testController.run(req, res);
 
-
-  expect(fn1).toHaveBeenCalledTimes(1)
-  expect(fn2).toHaveBeenCalledTimes(1)
-  expect(fn3).toHaveBeenCalledTimes(0)
-})
+  expect(fn1).toHaveBeenCalledTimes(1);
+  expect(fn2).toHaveBeenCalledTimes(1);
+  expect(fn3).toHaveBeenCalledTimes(0);
+});
