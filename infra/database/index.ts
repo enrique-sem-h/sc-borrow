ab2583001131800b59853d101d82a9f290b94ace
enrique-sem-h <carvalhoenrique33@gmail.com>
2026-06-17 21:07:49 -0300

    Hotfix: ssl certificate

--- infra/database/index.ts
+++ infra/database/index.ts
@@ -1,5 +1,5 @@
 import { drizzle } from "drizzle-orm/mysql2";
-import mysql from "mysql2/promise";
+import mysql, { SslOptions } from "mysql2/promise";
 import {
   anuncios,
   anunciosRelations,
@@ -12,12 +12,14 @@ import {
   usuarioRelations,
 } from "./schemas/alugueisSchema";
 import { usuarios } from "./schemas/usuariosSchema";
+import { boolean } from "zod";
 
-const config = {
+const config: mysql.PoolOptions = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: getSslValues(),
 };
 
 const poolConnection = mysql.createPool(config);
@@ -34,3 +36,15 @@ export const db = drizzle(poolConnection, {
   },
   mode: "default",
 });
+
+function getSslValues(): string | SslOptions | undefined {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+      rejectUnauthorized: true,
+    };
+  }
+  return process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: true }
+    : { rejectUnauthorized: false };
+}
